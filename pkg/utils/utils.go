0e0a1fe1bade074f78377d06e65475986e3992c7
vyom <sharm843@umn.edu>
2022-07-17 14:42:30 +0530

    Added parsing for req body function

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,1 +1,18 @@
 package utils
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+//Parsing request body to json
+func ParseBody(r *http.Request, x interface{}) {
+	//Reading the data from the request and unmarshalling it into json format, while saving it to a body slice
+	if body, err := ioutil.ReadAll(r.Body); err == nil {
+		if err := json.Unmarshal([]byte(body), x); err != nil {
+			//If error exists, then this stops the function
+			return
+		}
+	}
+}
